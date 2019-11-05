f79d01b8c93724fe3047fc0f439535c789d566f3
jeromeGsq <jerome.ghesquiere@gmail.com>
2019-11-05 10:26:11 +0100

    Add new content

---
+++ Editor/Gizmo/AnchorGizmo.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AnchorGizmo : MonoBehaviour
+{
+    public e_AnchorType AnchorType = e_AnchorType.Sphere;
+    public float SizeGizmo = 0.02f;
+    public Vector3 SizeRectangleGizmo;
+    public Color ColorGizmo = Color.white;
+
+    [Space(20)]
+
+    public bool ArrowDirection;
+    public float SizeArrow = 0.2f;
+
+    [SerializeField]
+    private bool isHandle;
+
+    private GUIStyle style;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = ColorGizmo;
+        switch (AnchorType)
+        {
+            case e_AnchorType.Cube:
+                Gizmos.DrawCube(transform.position, new Vector3(SizeGizmo, SizeGizmo, SizeGizmo));
+                break;
+
+            case e_AnchorType.Rectangle:
+                Gizmos.DrawCube(transform.position, new Vector3(SizeRectangleGizmo.x, SizeRectangleGizmo.y, SizeRectangleGizmo.z));
+                break;
+
+            case e_AnchorType.Sphere:
+                Gizmos.DrawSphere(transform.position, SizeGizmo);
+                break;
+
+            case e_AnchorType.BoxCollider:
+                var collider = this.GetComponent<BoxCollider2D>();
+                if (collider == null)
+                {
+                    collider = this.GetComponentInChildren<BoxCollider2D>();
+                }
+                Gizmos.DrawCube(transform.position, new Vector3(collider.size.x, collider.size.y, 1));
+                break;
+        }
+
+        if (ArrowDirection)
+        {
+            Gizmos.color = ColorGizmo;
+            Gizmos.DrawLine(transform.position, (transform.position - transform.forward * SizeArrow));
+            Gizmos.DrawLine((transform.position - transform.forward * SizeArrow), transform.position - (transform.right * SizeArrow / 2));
+            Gizmos.DrawLine((transform.position - transform.forward * SizeArrow), transform.position + (transform.right * SizeArrow / 2));
+        }
+    }
+}
+
+public enum e_AnchorType
+{
+    Cube,
+    Rectangle,
+    Sphere,
+    BoxCollider,
+}
---
+++ Runtime/Extensions/CoroutineUtils.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public static class CoroutineUtils
+{
+    public static Coroutine DelaySeconds(this MonoBehaviour mono, Action action, float delay, bool ignoreTimeScale = false)
+    {
+        return mono.StartCoroutine(DelaySeconds(action, delay, ignoreTimeScale));
+    }
+
+    private static IEnumerator DelaySeconds(Action action, float delay, bool ignoreTimeScale)
+    {
+        if (ignoreTimeScale)
+        {
+            yield return new WaitForSecondsRealtime(delay);
+        }
+        else
+        {
+            yield return new WaitForSeconds(delay);
+        }
+        action();
+    }
+}
